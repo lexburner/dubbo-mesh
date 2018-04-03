d91f3612cd104e06dcc01a62c5d10709c975cc84
雷威 <leiwei2094@gmail.com>
2018-04-03 16:32:40 +0800

    添加日志目录参数配置。 -Dlogs.dir=/path/to/your/logs/dir

--- mesh-provider/src/main/java/com/alibaba/dubbo/performance/demo/provider/ProviderApp.java
+++ mesh-provider/src/main/java/com/alibaba/dubbo/performance/demo/provider/ProviderApp.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class ProviderApp {
+    // 启动时请添加JVM参数:
+    // 日志保存目录: -Dlogs.dir=/path/to/your/logs/dir。请安装自己的环境来设置日志目录。
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderApp.class,args);
