3dfac68b09f091a91eebf8d0ee50dc3866c87d8f
雷威 <leiwei2094@gmail.com>
2018-04-04 13:53:42 +0800

    Provider去掉web容器，不监听8080web端口

--- mesh-provider/src/main/java/com/alibaba/dubbo/performance/demo/provider/ProviderApp.java
+++ mesh-provider/src/main/java/com/alibaba/dubbo/performance/demo/provider/ProviderApp.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @SpringBootApplication
 public class ProviderApp {
     // 启动时请添加JVM参数:
     // 日志保存目录: -Dlogs.dir=/path/to/your/logs/dir。请安装自己的环境来设置日志目录。
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(ProviderApp.class,args);
+        System.in.read();
     }
 }
