e117d31fa1dda24cab6030c11bbac0b90d6a6457
奥陌 <albert.tr@alibaba-inc.com>
2018-04-09 20:00:29 +0800

    Provider 启动时指定不同端口.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,13 @@ if [[ "$1" == "consumer" ]]; then
   java -jar -Xms2G -Xmx2G -Dlogs.dir=/root/logs /root/dists/mesh-consumer.jar
 elif [[ "$1" == "provider-small" ]]; then
   echo "Starting small provider service..."
-  java -jar -Xms1G -Xmx1G -Dlogs.dir=/root/logs /root/dists/mesh-provider.jar
+  java -jar -Xms1G -Xmx1G -Ddubbo.protocol.port=20889 -Dlogs.dir=/root/logs /root/dists/mesh-provider.jar
 elif [[ "$1" == "provider-medium" ]]; then
   echo "Starting medium provider service..."
-  java -jar -Xms2G -Xmx2G -Dlogs.dir=/root/logs /root/dists/mesh-provider.jar
+  java -jar -Xms2G -Xmx2G -Ddubbo.protocol.port=20890 -Dlogs.dir=/root/logs /root/dists/mesh-provider.jar
 elif [[ "$1" == "provider-large" ]]; then
   echo "Starting large provider service..."
-  java -jar -Xms3G -Xmx3G -Dlogs.dir=/root/logs /root/dists/mesh-provider.jar
+  java -jar -Xms3G -Xmx3G  -Ddubbo.protocol.port=20891 -Dlogs.dir=/root/logs /root/dists/mesh-provider.jar
 else
   echo "Unrecognized arguments, exit."
   exit 1
