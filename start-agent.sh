032bc39896ac803a1b1c20e90f8547890249b2ae
奥陌 <albert.tr@alibaba-inc.com>
2018-04-09 20:56:59 +0800

    保持 agent 在前台运行.

--- start-agent.sh
+++ start-agent.sh
@@ -8,51 +8,47 @@ echo ETCD_URL = $ETCD_URL
 
 if [[ "$1" == "consumer" ]]; then
   echo "Starting consumer agent..."
-  nohup java -jar \
-        -Xms1536M \
-        -Xmx1536M \
-        -Dtype=consumer \
-        -Dserver.port=20000 \
-        -Detcd.url=$ETCD_URL \
-        -Dlogs.dir=/root/logs \
-        /root/dists/mesh-agent.jar \
-        > /root/logs/agent.out 2>&1 &
+  java -jar \
+       -Xms1536M \
+       -Xmx1536M \
+       -Dtype=consumer \
+       -Dserver.port=20000 \
+       -Detcd.url=$ETCD_URL \
+       -Dlogs.dir=/root/logs \
+       /root/dists/mesh-agent.jar
 elif [[ "$1" == "provider-small" ]]; then
   echo "Starting small provider agent..."
-  nohup java -jar \
-        -Xms512M \
-        -Xmx512M \
-        -Dtype=provider \
-        -Dserver.port=30000 \
-        -Ddubbo.protocol.port=20889 \
-        -Detcd.url=$ETCD_URL \
-        -Dlogs.dir=/root/logs \
-        /root/dists/mesh-agent.jar \
-        > /root/logs/agent.out 2>&1 &
+  java -jar \
+       -Xms512M \
+       -Xmx512M \
+       -Dtype=provider \
+       -Dserver.port=30000 \
+       -Ddubbo.protocol.port=20889 \
+       -Detcd.url=$ETCD_URL \
+       -Dlogs.dir=/root/logs \
+       /root/dists/mesh-agent.jar
 elif [[ "$1" == "provider-medium" ]]; then
   echo "Starting medium provider agent..."
-  nohup java -jar \
-        -Xms1536M \
-        -Xmx1536M \
-        -Dtype=provider \
-        -Dserver.port=30001 \
-        -Ddubbo.protocol.port=20890 \
-        -Detcd.url=$ETCD_URL \
-        -Dlogs.dir=/root/logs \
-        /root/dists/mesh-agent.jar \
-        > /root/logs/agent.out 2>&1 &
+  java -jar \
+       -Xms1536M \
+       -Xmx1536M \
+       -Dtype=provider \
+       -Dserver.port=30001 \
+       -Ddubbo.protocol.port=20890 \
+       -Detcd.url=$ETCD_URL \
+       -Dlogs.dir=/root/logs \
+       /root/dists/mesh-agent.jar
 elif [[ "$1" == "provider-large" ]]; then
   echo "Starting large provider agent..."
-  nohup java -jar \
-        -Xms2560M \
-        -Xmx2560M \
-        -Dtype=provider \
-        -Dserver.port=30002 \
-        -Ddubbo.protocol.port=20891 \
-        -Detcd.url=$ETCD_URL \
-        -Dlogs.dir=/root/logs \
-        /root/dists/mesh-agent.jar \
-        > /root/logs/agent.out 2>&1 &
+  java -jar \
+       -Xms2560M \
+       -Xmx2560M \
+       -Dtype=provider \
+       -Dserver.port=30002 \
+       -Ddubbo.protocol.port=20891 \
+       -Detcd.url=$ETCD_URL \
+       -Dlogs.dir=/root/logs \
+       /root/dists/mesh-agent.jar
 else
   echo "Unrecognized arguments, exit."
   exit 1
