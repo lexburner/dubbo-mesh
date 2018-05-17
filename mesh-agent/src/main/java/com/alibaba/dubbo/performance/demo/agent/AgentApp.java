31eab5e9899b17c34f5720708e3f31849db2a759
kirito <250577914@qq.com>
2018-05-17 16:19:23 +0800

    etcd动态获取地址

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/AgentApp.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/AgentApp.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AgentApp {
     // agent会作为sidecar，部署在每一个Provider和Consumer机器上
-    // 在Provider端启动agent时，添加JVM参数-Dtype=provider -Dserver.port=30000 -Ddubbo.protocol.port=20889
-    // 在Consumer端启动agent时，添加JVM参数-Dtype=consumer -Dserver.port=20000
+    // 在Provider端启动agent时，添加JVM参数-Dtype=provider -Dserver.port=30000 -Ddubbo.protocol.port=20880 -Detcd.url=http://localhost:2379
+    // 在Consumer端启动agent时，添加JVM参数-Dtype=consumer -Dserver.port=20000 -Detcd.url=http://localhost:2379
     // 添加日志保存目录: -Dlogs.dir=/path/to/your/logs/dir。请安装自己的环境来设置日志目录。
 
     public static void main(String[] args) {
