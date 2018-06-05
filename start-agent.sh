2ac740539463731d624f2ae7fb5ee68a43482ccf
kirito <250577914@qq.com>
2018-06-05 15:24:46 +0800

    6 9 10负载，防止打爆provider

--- start-agent.sh
+++ start-agent.sh
@@ -22,7 +22,7 @@ elif [[ "$1" == "provider-small" ]]; then
        -Xms512M \
        -Xmx512M \
        -Dtype=provider \
-       -Dlb.weight=1\
+       -Dlb.weight=6\
        -Ddubbo.protocol.port=20880 \
        -Dserver.port=30000 \
        -Detcd.url=$ETCD_URL \
@@ -34,7 +34,7 @@ elif [[ "$1" == "provider-medium" ]]; then
        -Xms1536M \
        -Xmx1536M \
        -Dtype=provider \
-       -Dlb.weight=2\
+       -Dlb.weight=9\
        -Ddubbo.protocol.port=20880 \
        -Dserver.port=30000 \
        -Detcd.url=$ETCD_URL \
@@ -46,7 +46,7 @@ elif [[ "$1" == "provider-large" ]]; then
        -Xms2560M \
        -Xmx2560M \
        -Dtype=provider \
-       -Dlb.weight=2\
+       -Dlb.weight=10\
        -Ddubbo.protocol.port=20880 \
        -Dserver.port=30000 \
        -Detcd.url=$ETCD_URL \
